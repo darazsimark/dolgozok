4011f34100985ba9949f267155b051f057d5213d
Darázsi Márk <t1@122HP4>
2018-02-03 10:50:36 +0100

    Fizetésemelés-Törlés.

--- src/dolgozok/Dolgozok.java
+++ src/dolgozok/Dolgozok.java
@@ -52,6 +52,18 @@ public class Dolgozok {
                     fnev = bill.nextLine();
                     ab.kiir(fnev);
                     break;
+                case '5':
+                    System.out.print("Százalék: ");
+                    int sz = bill.nextInt();
+                    bill.nextLine();
+                    ab.emel(sz);
+                    break;
+                case '6':
+                    System.out.print("Törölni szeretnél minden rekordot?(i/n): ");
+                    char igen = bill.nextLine().charAt(0);
+                    if (igen == 'i')
+                        ab.torol();
+                    break;
                 default:
                     tovabb = false;
             }
