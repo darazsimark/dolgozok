a2908890c789441eab80ff929333e54635a3f3b1
Darázsi Márk <t1@122HP4>
2018-02-03 09:27:38 +0100

    Kapcsolódás.

--- src/dolgozok/DB.java
+++ src/dolgozok/DB.java
@@ -6,8 +6,10 @@
 package dolgozok;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
@@ -22,4 +24,14 @@ public class DB {
             + "?useUnicode=true&characterEncoding=UTF-8";
     final String user = "root";
     final String pass = "";
+    
+    public DB() {
+        try {
+            // kapcsolódás
+            kapcs = DriverManager.getConnection(dbUrl, user, pass);
+            System.out.println("A kapcsolat létrejött.");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
