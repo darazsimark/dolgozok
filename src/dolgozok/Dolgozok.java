9efa21020eb306c7eb5f48d39b5e1b407f20bba1
Darázsi Márk <t1@122HP4>
2018-02-03 10:25:47 +0100

    menük létrehozása. Listázás, új dolgozók hozzáadása menüpontok készek.

--- src/dolgozok/Dolgozok.java
+++ src/dolgozok/Dolgozok.java
@@ -5,6 +5,8 @@
  */
 package dolgozok;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Darázsi Márk
@@ -16,6 +18,34 @@ public class Dolgozok {
      */
     public static void main(String[] args) {
         DB ab = new DB();
+        Scanner bill = new Scanner(System.in,"cp1250");
+        boolean tovabb = true;
+        String nev, szulido, fnev;
+        int fizetes;
+        
+        while (tovabb) {
+            System.out.println("1-Listázás 2-Új 3-Importálás 4-Exportálás "
+                             + "5-Emelés 6-Törlés 0-Kilépés");
+            System.out.print("Válasz: ");
+            String v = bill.nextLine();
+            switch (v.charAt(0)) {
+                case '1':
+                    ab.lista();
+                    break;
+                case '2':
+                    System.out.print("Név: ");
+                    nev = bill.nextLine();
+                    System.out.print("Születési idő: ");
+                    szulido = bill.nextLine();
+                    System.out.print("Fizetés: ");
+                    fizetes = bill.nextInt();
+                    bill.nextLine();
+                    ab.uj(nev, szulido, fizetes);
+                    break;
+                default:
+                    tovabb = false;
+            }
+        }
     }
     
 }
